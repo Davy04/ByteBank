819e24e1ce3de801af91620c437cc38d53a3b189
Davy Woolley <davywoolleyramos22@gmail.com>
2022-10-17 16:34:23 -0300

    att bytebank

--- bytebank/Program.cs
+++ bytebank/Program.cs
@@ -8,3 +8,18 @@ contaDoAndre.saldo = 100;
 
 Console.WriteLine("Saldo da conta do André = " + contaDoAndre.saldo);
 
+contaDoAndre.depositar(100);
+
+Console.WriteLine("Saldo da conta pós deposito = " + contaDoAndre.saldo);
+
+contaDoAndre.sacar(50);
+
+if (contaDoAndre.sacar(300) == true)
+{
+    Console.WriteLine("saldo da conta pós saque = " + contaDoAndre.saldo);
+} 
+else
+{
+    Console.WriteLine("saldo insuficiente");
+}
+ 
--- bytebank/contaCorrente.cs
+++ bytebank/contaCorrente.cs
@@ -16,6 +16,33 @@ namespace bytebank
         public void depositar(double valor)
         {
             this.saldo += valor;
+
+        }
+        public bool sacar(double valor)
+        {
+            if (valor <= this.saldo)
+            {
+                this.saldo -= valor;
+                return true;
+            } 
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool transferir(double valor, ContaCorrente destino)
+        {
+            if (this.saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                this.saldo -= valor;
+                destino.saldo += valor;
+                return true;
+            }
         }
     }
 }
