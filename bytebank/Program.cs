fbd196149b54d95145232892752f619bf8387699
Davy Woolley <davywoolleyramos22@gmail.com>
2022-10-19 16:50:13 -0300

    class e motodos

--- bytebank/Program.cs
+++ bytebank/Program.cs
@@ -28,21 +28,39 @@ using bytebank.titular;
 //contaDoPedro.conta = "1010-5";
 //contaDoPedro.saldo = 350;
 
-Cliente cliente = new Cliente();
-cliente.nome = "André Silva";
-cliente.cpf = "123456789";
-cliente.profissao = "analista";
+//Cliente cliente = new Cliente();
+//cliente.nome = "André Silva";
+//cliente.cpf = "123456789";
+//cliente.profissao = "analista";
 
-ContaCorrente conta = new ContaCorrente();
-conta.titular = cliente;
-conta.conta = "1010-x";
-conta.numero_agencia = 15;
-conta.saldo = 100;
+//ContaCorrente conta = new ContaCorrente();
+//conta.titular = cliente;
+//conta.conta = "1010-x";
+//conta.numero_agencia = 15;
+//conta.saldo = 100;
 
-Console.WriteLine("titular : " + conta.titular.nome);
-Console.WriteLine("CPF: "+ conta.titular.cpf);
-Console.WriteLine("Profissão: "+ conta.titular.profissao);
-Console.WriteLine("N° conta: "+ conta.conta);
-Console.WriteLine("Saldo: "+ conta.saldo);
-Console.WriteLine("N° agência: "+ conta.numero_agencia);
+//Console.WriteLine("titular : " + conta.titular.nome);
+//Console.WriteLine("CPF: "+ conta.titular.cpf);
+//Console.WriteLine("Profissão: "+ conta.titular.profissao);
+//Console.WriteLine("N° conta: "+ conta.conta);
+//Console.WriteLine("Saldo: "+ conta.saldo);
+//Console.WriteLine("N° agência: "+ conta.numero_agencia);
 
+//ContaCorrente conta2 = new ContaCorrente();
+//conta2.titular = new Cliente();
+//conta2.titular.nome = "josé Souza";
+//conta2.titular.profissao = "tester";
+//conta2.titular.cpf = "987654321";
+//conta2.conta = "9999-x";
+//conta2.numero_agencia = 18;
+//conta2.saldo = 500;
+
+//Console.WriteLine("titular: " + conta2.titular.nome);
+
+ContaCorrente conta3 = new ContaCorrente();
+conta3.SetSaldo(200);
+conta3.Numero_agencia = 18;
+conta3.Conta = "1011-H";
+Console.WriteLine(conta3.GetSaldo());
+Console.WriteLine(conta3.Numero_agencia);
+Console.WriteLine(conta3.Conta);
--- bytebank/contas/contaCorrente.cs
+++ bytebank/contas/contaCorrente.cs
@@ -9,11 +9,25 @@ namespace bytebank.contas
 {
     public class ContaCorrente
     {
-        public int numero_agencia;
-        public string conta;
-        public double saldo = 100;
+        private int numero_agencia;
+         
+        public int Numero_agencia
+        {
+            get { return this.numero_agencia; }
+            set { 
+                    if(value > 0)
+                    {
+                        this.numero_agencia = value; 
+                    }
+                }
+        }
 
-        public Cliente titular;
+        //private string conta;
+        public string Conta { get; set; }
+
+        private double saldo = 100;
+
+        private Cliente titular;
 
         public void depositar(double valor)
         {
@@ -47,12 +61,21 @@ namespace bytebank.contas
             }
         }
 
-        public void mostrar()
+        public void SetSaldo(double valor)
         {
-            Console.WriteLine("titular: " + titular);
-            Console.WriteLine("conta: " + conta);
-            Console.WriteLine("número agencia: " + numero_agencia);
-            Console.WriteLine("saldo: " + saldo);
+            if(valor < 0 )
+            {
+                return;
+            } 
+            else
+            {
+                this.saldo = valor;
+            }
+        } 
+        
+        public double GetSaldo()
+        {
+            return this.saldo;
         }
     }
 }
